3f92936773579551cab4bf9beea1d3392309a6ff
trungboro116 <trungboro116@gmail.com>
2024-12-01 20:06:43 +0700

    search

---
+++ js/searchbarbentrai.js
@@ -0,0 +1,76 @@
+
+const products = [
+    {
+      name: "Piqué Biker Jacket",
+      price: "$67.24",
+      image: "img/product/product-2.jpg",
+      link: "shop-details-1.html"
+    },
+    {
+      name: "Black Sneaker",
+      price: "$43.48",
+      image: "img/product/product-3.jpg",
+      link: "shop-details-2.html"
+    },
+    {
+      name: "Brown Hooded Shirt Jacket",
+      price: "$60.9",
+      image: "img/product/product-4.jpg",
+      link: "shop-details-3.html"
+    },
+    {
+      name: "Gray Fringed Scarf",
+      price: "$98.49",
+      image: "img/product/product-6.jpg",
+      link: "shop-details-4.html"
+    },
+    {
+      name: "Fringed Black Scarf",
+      price: "$85.99",
+      image: "img/product/product-7.jpg",
+      link: "shop-details-5.html"
+    },
+    {
+        name:"Black Floral Graphic T-shirt",
+        price: "$67.24",
+        image: "img/product/product-9.jpg",
+        link: "shop-details-7.html"
+    },
+    {
+        name:"Perfume Set",
+        price:"$43.48",
+        image:"img/product/product-10.jpg",
+        link:"shop-details-8.html"
+    },
+    {
+        name:"Navy Henley T-shirt",
+        price:"$26.28",
+        image:"img/product/product-8.jpg",
+        link:"shop-details-6.html"
+    },
+    {
+        name:"White Utility Backpack",
+        price:"$60.9",
+        image:"img/product/product-11.jpg",
+        link:"shop-details-9.html"
+    },
+    {
+        name:"Camouflage Hooded Jacket",
+        price:"$98.49",
+        image:"img/product/product-12.jpg",
+        link:"shop-details-10.html"
+    },
+    {
+        name:"Brown Leather Backpack",
+        price:"$49.66",
+        image:"img/product/product-13.jpg",
+        link:"shop-details-11.html"
+    },
+    {
+        name:"Gold Rectangular Cufflinks",
+        price:"$26.28",
+        image:"img/product/product-14.jpg",
+        link:"shop-details-12.html"
+    },
+  ];
+
